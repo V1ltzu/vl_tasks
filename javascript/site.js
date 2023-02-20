0e785d75940a7a94171bef1f79eed20e7476d8ff
V1ltzu <villelehtonen1@gmail.com>
2023-02-20 11:17:42 +0200

    javascitp¨r

---
+++ javascript/site.js
@@ -0,0 +1,15 @@
+function hello(){
+    alert("Moi");
+}
+
+function add(){
+    document.write(5+6);
+}
+
+function changeColor(background, newColor){
+    if (where == "newColor"){
+        document.body.style.backgroundColor = newColor;
+    }
+}
+
+//document.getElementById(id)
--- layout/ex1.php
+++ layout/ex1.php
@@ -29,6 +29,17 @@ echo "Hello World! My name is \"David\"";
 
 <h2>3.3 Write the PHP code in to display the current date.</h2>
 
+<script>
+  document.write("Hello world blablalblasl");
+</script>
+<noscript>
+  Please enable JavaScript to blablabla
+</noscript>
+<input type="button" onclick="hello()" value="Click Me">
+<script>
+  window.alert("This triggers and alert");
+  window.alert(5+10);
+</script>
 <?php
 echo date("D.d.M.Y")
   ?>
@@ -67,6 +78,18 @@ echo date("D.d.M.Y")
     <h2>4. Take a screenshot that verifies, you have set up the development environment and include it as an image in ex1.php. </h2>
 
     <img src="photo.png" alt="photo" width="700px" height="300px">
+
+  <button onclick="add()">Click to add</button>
+  <hr>
+  <p id="place1" style="color:red; background-color:yellow;"> </p>
+  <span id="place2" style="color:red; background-color:green;"> </span>
+  <script>
+    document.getElementById("place1").innerHTML = "This will go to place1";
+    document.getElementById("place2").innerHTML = "This will go to span";
+
+    <h2>Changing background-color</h2>
+    <form>
+    <input type="color" name >
 
     <?php
     include "footer.php"
